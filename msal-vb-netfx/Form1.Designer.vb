434ae09d315305f74ec59ba9d361083fabf7bf48
Alfredo Revilla <alfredorevilla@gmail.com>
2022-04-26 16:31:23 -0500

    done

--- msal-vb-netfx/Form1.Designer.vb
+++ msal-vb-netfx/Form1.Designer.vb
@@ -22,10 +22,42 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(288, 182)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(101, 45)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Login"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(36, 72)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 16)
+        Me.Label1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(678, 284)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.DoubleBuffered = True
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
 End Class
--- msal-vb-netfx/Form1.vb
+++ msal-vb-netfx/Form1.vb
@@ -1,3 +1,14 @@
-﻿Public Class Form1
+﻿Imports System.Configuration
+Imports Microsoft.Identity.Client
 
+Public Class Form1
+
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim app As IPublicClientApplication = PublicClientApplicationBuilder.Create(ConfigurationManager.AppSettings.Item("clientId")).WithDefaultRedirectUri().Build()
+        Dim result = Await app.AcquireTokenInteractive(New String() {}).ExecuteAsync().ConfigureAwait(False)
+
+        Dim name = result.ClaimsPrincipal.FindFirst("name").Value
+        Label1.Text = $"You logged in as {result.Account.Username}{If(String.IsNullOrWhiteSpace(name), "", $" ({name})")}"
+
+    End Sub
 End Class
